b2d2b2b07a2a198233578fb5799a12c2b0317ce7
Rodney Wormsbecher <rodney22@live.nl>
2022-12-06 13:57:07 +0100

    initial app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <main>
+            <h1>The Offspring</h1>
+            <p>On tour: yes</p>
+
+            <p>2022-2023 tour dates: 21 concerts</p>
+            <p>Next concert near you: 5 months (Brussels)</p>
+
+            <p>
+                1,071,250 fans get concert alerts for this artist. Join Songkick to track The Offspring and get concert
+                alerts when they play near you.
+            </p>
+
+            <button>YES, PLEASE NOTIFY ME</button>
+        </main>
+    );
 }
 
 export default App;
